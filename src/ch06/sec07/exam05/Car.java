0627d35ce841e2374eef0f4997a1b8992f35cee9
shkim00216 <shkim00216@gmail.com>
2024-08-29 02:20:30 +0900

    08290220

--- src/ch06/sec07/exam05/Car.java
+++ src/ch06/sec07/exam05/Car.java
@@ -7,16 +7,14 @@ public class Car {
 	String color;
 	int maxSpeed;
 	
-	//생성자 선언
-	Car() {}
-	
 	Car(String model) {
-		this.model = model;
+		//20라인 생성자 호출
+		this(model, "은색", 250);
 	}
 	
 	Car(String model, String color) {
-		this.model = model;
-		this.color = color;
+		//20라인 생성자 호출
+		this(model, color, 250);
 	}
 	
 	Car(String model, String color, int maxSpeed) {
